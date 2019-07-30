f4034cc5de2b8e66e55156686347ceaf0ba03d50
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-30 13:29:37 -0500

    Initial commit. Setup html and basic css/js to display game board

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+
+// returns the string of HTML to render the game blocks into
+const createGameSquareComponent = (id) => {
+    return `
+        <section class="square" id="${id}"></section>
+    `
+}
+
+const gameBoard = document.querySelector("#game__mainTag")
+
+
+
+
+
+// RENDER the squares to the DOM
+const renderSquaresToDOM = () => {
+    for (let index = 0; index < 9; index++) {
+        const newSquare = createGameSquareComponent(index)
+        gameBoard.innerHTML += newSquare
+    }
+} 
+
+renderSquaresToDOM()
